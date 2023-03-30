083ef691bca35f2c5e2ae2c50419b460f8c7fc5c
Albin <Albin@DESKTOP-2LCI1U3>
2023-03-30 07:46:05 +0200

    Projektdateien hinzufügen.

---
+++ Aufgabe 24.03.2023/Program.cs
@@ -0,0 +1,23 @@
+﻿int var1 = 1;
+int var2 = 1;
+int var3 = 1;
+
+
+Console.WriteLine("Wie weit soll die Rechnung gehen?");
+int ans = Convert.ToInt32(Console.ReadLine());
+
+
+
+for (int i = 0; i < ans; i++)
+{
+    var3 = var1 + var2;
+    Console.WriteLine("{0} + {1} = {2}", var2, var1, var3);
+    var2 = var1 + var3;
+    Console.WriteLine("{0} + {1} = {2}", var1, var3, var2);
+    var1 = var2 + var3;
+    Console.WriteLine("{0} + {1} = {2}", var2, var1, var1);
+}
+
+
+
+
