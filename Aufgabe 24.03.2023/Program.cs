71c43b31aa667d25b1113727e9c19db6a64b1238
Albin <Albin@DESKTOP-2LCI1U3>
2023-03-30 07:49:20 +0200

    mit Kommentare

--- Aufgabe 24.03.2023/Program.cs
+++ Aufgabe 24.03.2023/Program.cs
@@ -1,7 +1,7 @@
 ﻿int var1 = 1;
 int var2 = 1;
 int var3 = 1;
-
+//Zuerst werden Variablen für die Zahlen erstellt
 
 Console.WriteLine("Wie weit soll die Rechnung gehen?");
 int ans = Convert.ToInt32(Console.ReadLine());
@@ -10,10 +10,13 @@ int ans = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 0; i < ans; i++)
 {
+    //Hier wird jede Variable benützt und in verschiedenen Formen gerechnet
     var3 = var1 + var2;
     Console.WriteLine("{0} + {1} = {2}", var2, var1, var3);
+    //Hier wird von oben die neuen Zahlen genommen und dann bei dieser Rechnung benützt
     var2 = var1 + var3;
     Console.WriteLine("{0} + {1} = {2}", var1, var3, var2);
+    //Hier genau das gleiche
     var1 = var2 + var3;
     Console.WriteLine("{0} + {1} = {2}", var2, var1, var1);
 }
